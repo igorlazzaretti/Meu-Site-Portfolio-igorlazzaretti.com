5ba45a4d3863d90e032e3ecd9130f189f72c9cfa
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2023-10-30 14:08:44 -0300

    feito

--- projects/Pedido-Irrecusável/scrypt.js
+++ projects/Pedido-Irrecusável/scrypt.js
@@ -1,28 +1,28 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  const naoButton = document.getElementById("nao");
-  const simButton = document.getElementById("sim");
+    const naoButton = document.getElementById("nao");
+    const simButton = document.getElementById("sim");
 
-  simButton.addEventListener("click", function () {
-    document.getElementById("sim").innerHTML = "Resposta Certa =D ";
+    simButton.addEventListener("click", function () {
+        document.getElementById("sim").innerHTML = "Resposta Certa =D ";
     });
-  naoButton.addEventListener("click", function () {
-    document.getElementById("nao").innerHTML = "Resposta Errada, tente a outra!";
+    naoButton.addEventListener("click", function () {
+        document.getElementById("nao").innerHTML = "Resposta Errada, tente a outra!";
     });
-  
-// 
-  const handleNao = function () {
-    naoButton.style.position = "fixed";
 
-    const randTop =
-      Math.random() * (window.innerHeight - naoButton.offsetHeight);
-    const randLeft =
-      Math.random() * (window.innerWidth - naoButton.offsetWidth);
+    // 
+    const handleNao = function () {
+        naoButton.style.position = "fixed";
 
-    naoButton.style.top = randTop + "px";
-    naoButton.style.left = randLeft + "px";
-  };
+        const randTop =
+            Math.random() * (window.innerHeight - naoButton.offsetHeight);
+        const randLeft =
+            Math.random() * (window.innerWidth - naoButton.offsetWidth);
 
-  naoButton.addEventListener("click", handleNao);
-  naoButton.addEventListener("mouseover", handleNao);
+        naoButton.style.top = randTop + "px";
+        naoButton.style.left = randLeft + "px";
+    };
+
+    naoButton.addEventListener("click", handleNao);
+    naoButton.addEventListener("mouseover", handleNao);
 });
